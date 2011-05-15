561be7fde1ba251e7a8d3f8faf0aad1fb7169376
nisbus <nisbus@gmail.com>
2011-05-15 07:27:03 +0000

    Added documentation and specs

--- src/stomp_client.erl
+++ src/stomp_client.erl
@@ -1,8 +1,9 @@
 %%%-------------------------------------------------------------------
-%%% @author nisbus
-%%% @copyright (C) 2011, nisbus 
+%%% @author nisbus <nisbus@kodiak.is>
+%%% @author Omar Yasin <omarkj@kodiak.is>
+%%% @copyright (C) 2011, Kodi ehf (http://kodiak.is)
 %%% @doc
-%%%
+%%% A stomp client gen_server for Erlang
 %%% @end
 %%% Created : 13 May 2011 by nisbus
 %%%-------------------------------------------------------------------
@@ -40,44 +41,65 @@
 %%%===================================================================
 %%% API
 %%%===================================================================
+%%% @doc Starts the client
+-spec start(string(),integer(),string(),string(),fun()) -> {ok,pid}.		   
 start(Host,Port,User,Pass,MessageFunc) ->
     start_link(Host,Port,User,Pass,MessageFunc).
 
+%%% @doc Stops the client
+-spec stop(pid) -> ok.
 stop(Pid) ->
     gen_server:cast(Pid,{stop}).
     
+%%% @doc Subscribe to a single topic
+-spec subscribe_topic(string(),[tuple(string(),string())],pid) -> ok.
 subscribe_topic(Topic,Options,Pid) ->
     gen_server:cast(Pid, {subscribe,topic,Topic,Options}).
 
+%%% @doc Subscribe to a single queue
+-spec subscribe_queue(string(),[tuple(string(),string())],pid) -> ok.
 subscribe_queue(Queue,Options,Pid) ->
     gen_server:cast(Pid, {subscribe,queue,Queue,Options}).
 
+%%% @doc unsubscribe from a single topic
+-spec unsubscribe_topic(string(),pid) -> ok.
 unsubscribe_topic(Topic,Pid) ->
     gen_server:cast(Pid, {unsubscribe,topic,Topic}).
 
+%%% @doc unsubscribe from a single queue
+-spec unsubscribe_queue(string(),pid) -> ok.
 unsubscribe_queue(Queue,Pid) ->
     gen_server:cast(Pid, {unsubscribe,queue,Queue}).
 
+%%% @doc ack a message using the message or the message id
+-spec ack(string(),pid) -> ok.
 ack(Message,Pid) ->
     gen_server:cast(Pid,{ack, Message}).
 
+%%% @doc ack a message from a transaction using the message or the message id and the transaction id
+-spec ack(string(),string(),pid) -> ok.
 ack(Message, TransactionId,Pid) ->
     gen_server:cast(Pid, {ack, Message,TransactionId}).
 
+%%% @doc send a message to a topic
+-spec send_topic(string(),string(),[tuple(string(),string())],pid) -> ok.
 send_topic(Topic, Message,Options,Pid) -> 
     io:format("Sending to topic~p~n",[Topic]),
     gen_server:cast(Pid, {send, topic, {Topic,Message,Options}}).
 
+%%% @doc send a message to a queue
+-spec send_queue(string(),string(),[tuple(string(),string())],pid) -> ok.
 send_queue(Queue, Message,Options,Pid) ->
     gen_server:cast(Pid, {send, queue, {Queue,Message,Options}}).
 
+%%% @hidden
 start_link(Host,Port,User,Pass,MessageFunc) ->
     gen_server:start_link(?MODULE, [{Host,Port,User,Pass,MessageFunc}], []).
 
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
-
+%%% @hidden
 init([{Host,Port,User,Pass,F}]) ->
     Message=lists:append(["CONNECT", "\nlogin: ", User, "\npasscode: ", Pass, "\n\n", [0]]),
     {ok,Sock}=gen_tcp:connect(Host,Port,[{active, false}]),
@@ -87,34 +109,40 @@ init([{Host,Port,User,Pass,F}]) ->
     inet:setopts(Sock,[{active,once}]),
     {ok, #state{framer = State, socket = Sock, onmessage = F}}.
 
+%%% @hidden
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
+%%% @hidden
 handle_cast({subscribe, topic ,Topic, Options}, #state{socket = Sock} = State) ->
     Message = lists:append(["SUBSCRIBE", "\ndestination: ", "/topic/"++Topic,format_options(Options) ,"\n\n", [0]]),
     gen_tcp:send(Sock,Message),   
     inet:setopts(Sock,[{active,once}]),
     {noreply, State#state{subscriptions = [State#state.subscriptions|Topic]}};
 
+%%% @hidden
 handle_cast({subscribe, queue ,Queue,Options}, #state{socket = Sock} = State) ->
     Message = lists:append(["SUBSCRIBE", "\ndestination: ", "/queue/"++Queue,format_options(Options) ,"\n\n", [0]]),
     gen_tcp:send(Sock,Message),   
     inet:setopts(Sock,[{active,once}]),
     {noreply, State#state{subscriptions = [State#state.subscriptions|Queue]}};
 
+%%% @hidden
 handle_cast({unsubscribe, topic ,Topic}, #state{socket = Sock} = State) ->
     Message=lists:append(["UNSUBSCRIBE", "\ndestination: ", "/topic/"++Topic, "\n\n", [0]]),
     gen_tcp:send(Sock,Message),   
     inet:setopts(Sock,[{active,once}]),
     {noreply, State#state{subscriptions = [State#state.subscriptions|Topic]}};
 
+%%% @hidden
 handle_cast({unsubscribe, queue ,Queue}, #state{socket = Sock} = State) ->
     Message=lists:append(["UNSUBSCRIBE", "\ndestination: ", "/queue/"++Queue, "\n\n", [0]]),
     gen_tcp:send(Sock,Message),   
     inet:setopts(Sock,[{active,once}]),
     {noreply, State#state{subscriptions = [State#state.subscriptions|Queue]}};
 
+%%% @hidden
 handle_cast(stop,#state{socket =Sock } = State) ->
     Message = lists:append(["DISCONNECT","\n\n",[0]]),
     gen_tcp:send(Sock,Message),
@@ -122,6 +150,7 @@ handle_cast(stop,#state{socket =Sock } = State) ->
     gen_tcp:close(Sock),
     {stop, normal,State};
 
+%%% @hidden
 handle_cast({ack, Message},#state{socket = Socket} = State) ->
     MessageId = case Message of
 		    [_Type, {header,Headers}, _Body] ->
@@ -134,6 +163,7 @@ handle_cast({ack, Message},#state{socket = Socket} = State) ->
     inet:setopts(Socket,[{active,once}]),
     {noreply,State};
 
+%%% @hidden
 handle_cast({ack, Message,TransactionId},#state{socket = Socket} = State) ->
     MessageId = case Message of
 		    [_Type, {header,Headers}, _Body] ->
@@ -146,21 +176,25 @@ handle_cast({ack, Message,TransactionId},#state{socket = Socket} = State) ->
     inet:setopts(Socket,[{active,once}]),
     {noreply,State};
 
+%%% @hidden
 handle_cast({send, topic, {Topic, Message,Options}}, #state{socket = Socket} = State) ->
     Msg = lists:append(["SEND","\ndestination:","/topic/"++Topic, format_options(Options),"\n\n",Message,[0]]),
     gen_tcp:send(Socket,Msg),
     inet:setopts(Socket,[{active,once}]),    
     {noreply,State};
 
+%%% @hidden
 handle_cast({send, queue, {Queue, Message,Options}},#state{socket = Socket} = State) ->
     Msg = lists:append(["SEND","\ndestination:","/queue/"++Queue, format_options(Options),"\n\n",Message,[0]]),
     gen_tcp:send(Socket,Msg),
     inet:setopts(Socket,[{active,once}]),    
     {noreply,State};
 
+%%% @hidden
 handle_cast(_Msg, State) ->    
     {noreply, State}.
 
+%%% @hidden
 handle_info(_Info, #state{socket = Sock, onmessage = Func} = State) ->
     {_,_,Data} = _Info,    
     NewState = case frame(Data,State#state.framer) of
@@ -176,9 +210,11 @@ handle_info(_Info, #state{socket = Sock, onmessage = Func} = State) ->
     inet:setopts(Sock,[{active,once}]),
     {noreply, State#state{framer = NewState}}.
 
+%%% @hidden
 terminate(_Reason, _State) ->
     ok.
 
+%%% @hidden
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
@@ -247,7 +283,7 @@ format_options(Options) ->
 				throw("Error: invalid options format: "++E) 
 			end
 		end,[],Options).
-
+%%%@hidden
 test() ->
     Msg = "CONNECTED\n"++
 "session:ID:id:ID"++
