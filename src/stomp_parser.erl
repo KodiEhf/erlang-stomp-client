26360e5529de1693dd43b923d038d0793fe45e23
Ómar Kjartan Yasin <omarkj@gmail.com>
2011-05-15 17:29:09 +0000

    Speced up the parser and added types.

--- src/stomp_parser.erl
+++ src/stomp_parser.erl
@@ -16,11 +16,16 @@
 
 
 %% API
+-spec frame(Buffer::string()) -> FramedMessage::#framer_state{}.
 frame(Buffer) ->
     frame(Buffer, #framer_state{}).
+
+-spec parse(Message::string()) -> message().
 parse(Message) ->
     parse(Message, #parser_state{}).
 
+%% Framer
+-spec frame([] | string(), #framer_state{}) -> FramedMessage::#framer_state{}.
 frame([],State) ->
     State;
 frame([First|Rest],#framer_state{current = [], messages = []}) ->
@@ -36,6 +41,8 @@ frame([Match|Rest], #framer_state{current = Current} = State) ->
 frame([Match|[]], #framer_state{current = Current} = State) ->
     State#framer_state{current = Current++[Match]}.
 
+%% Parser
+-spec parse(string(), #parser_state{}) -> message().
 %First character
 parse([First|Rest], #parser_state{last_char = undefined} = State) ->
     parse(Rest, State#parser_state{last_char = First, current = [First]});
@@ -75,23 +82,23 @@ parse([First|Rest], #parser_state{last_char = Last, current = Current} = State) 
 %% Test
 test() ->
     Msg = "CONNECTED\n"++
-"session:ID:id:ID"++
-"\n\n"++[0]++
-"MESSAGE\n"++
-"message-id:ID:staging-53630-634408209863540308-1:1:1:1:2970112"++[10]++
-"destination:/topic/MARKET.DATA\n"++
-"timestamp:1305323578525"++[10]++
-"expires:0\n"++
-"content-length:814\n"++
-"priority:4\n\n"++
-"TEST"++[0]++"MESSAGE\n"++
-"message-id:ID:staging-53630-634408209863540308-1:1:1:1:2970112"++[10]++
-"destination:/topic/MARKET.DATA\n"++
-"timestamp:1305323578525"++[10]++
-"expires:0\n"++
-"content-length:814\n"++
-"priority:4\n\n"++
-"TEST"++[0],
+	"session:ID:id:ID"++
+	"\n\n"++[0]++
+	"MESSAGE\n"++
+	"message-id:ID:staging-53630-634408209863540308-1:1:1:1:2970112"++[10]++
+	"destination:/topic/MARKET.DATA\n"++
+	"timestamp:1305323578525"++[10]++
+	"expires:0\n"++
+	"content-length:814\n"++
+	"priority:4\n\n"++
+	"TEST"++[0]++"MESSAGE\n"++
+	"message-id:ID:staging-53630-634408209863540308-1:1:1:1:2970112"++[10]++
+	"destination:/topic/MARKET.DATA\n"++
+	"timestamp:1305323578525"++[10]++
+	"expires:0\n"++
+	"content-length:814\n"++
+	"priority:4\n\n"++
+	"TEST"++[0],
     F = frame(Msg,#framer_state{}),
     lists:foreach(fun(X) ->
 			  io:format("~p~n",[parse(X,#parser_state{})])
--- src/stomp_parser.hrl
+++ src/stomp_parser.hrl
@@ -1,14 +1,19 @@
+%% Copyright (c) 2011, Kodi ehf (http://kodiak.is)
+
+-type header() :: {string(), string()}.
+
+-type message() :: [{type | body, string()} |
+		    {header, header()}].
+
 -record(framer_state,
-	{
-	  current = [] :: [char()],
-	  messages = [] :: [any()]
+	{current = [] :: string(),
+	 messages = [] :: [message()]
 	}).
 -record(parser_state,
-	{
-	  current = [] :: [char()],
-	  last_char :: char(),
-	  key = [] :: [char()],
-	  message = [] :: [any()],
-	  got_type = false :: boolean(),
-	  header = [] :: [any()]
+	{current = [] :: string(),
+	 last_char :: string(),
+	 key = [] :: string(),
+	 message = [] :: [message()],
+	 got_type = false :: boolean(),
+	 header = [] :: [header()]
 	}).
